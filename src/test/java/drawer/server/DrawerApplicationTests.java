1db6c361ac6c78b9e806a51bfddb23f4c24bcacb
Choi Hong Gyu <zxolqs@gmail.com>
2024-01-25 23:13:59 +0900

    init project

---
+++ src/test/java/drawer/server/DrawerApplicationTests.java
@@ -0,0 +1,13 @@
+package drawer.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
