0ed2bedf4e1f3fa2370fb491af2f23e71237630d
Choi Hong Gyu <zxolqs@gmail.com>
2024-01-29 10:16:58 +0900

    add user repository interface

---
+++ src/main/java/drawer/server/domain/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package drawer.server.domain.user.repository;
+
+import drawer.server.domain.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Void removeByEmail(String email);
+}
